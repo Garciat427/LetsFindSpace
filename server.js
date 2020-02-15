4d8a5370cddbc824d70dd7a6f45c29e13d46351b
viet-quocnguyen <viet.nguyen@ceridian.com>
2020-02-15 04:21:45 -0500

    add packages

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const path = require("path");
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8000;
 const app = express();
+
+var bodyParser = require("body-parser");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Send every request to the React app
-// Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+app.post("/name", (req, res) => {
+  res.json({
+    name: req.body.name
+  });
 });
 
 app.listen(PORT, function() {
