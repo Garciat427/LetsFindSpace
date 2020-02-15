09fb570547cacb6141dab52e21da664179b36da8
Bassel Ashi <45138315+BasselAshi@users.noreply.github.com>
2020-02-15 01:59:07 -0500

    >Cleaned code and made a prototype for post

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 const express = require("express");
-const path = require("path");
-const PORT = process.env.PORT || 8000;
 const app = express();
+const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 8000;
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Send every request to the React app
-// Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+app.post("/addNumbers", (req, res) => {
+  var numOne = req.body.numOne;
+  var numTwo = req.body.numTwo;
+  var sum = parseInt(numOne) + parseInt(numTwo);
+  res.json({ sum: "hi", hamada: "hello" });
 });
 
 app.listen(PORT, function() {
