81f810465aac2a253ff70a106961e0259fa8cca9
Bassel Ashi <45138315+BasselAshi@users.noreply.github.com>
2020-02-16 07:26:55 -0500

    Fixed findNearestPlace

--- controllers/dbController.js
+++ controllers/dbController.js
@@ -4,8 +4,7 @@ const MeetingPlace = require("../models/meetingPlace");
 const fetch = require("node-fetch");
 const generalController = require("../controllers/generalController");
 
-// Helper functions
-const getGeo = async function(address) {
+const getGeo = async function (address) {
   address = address.replace(" ", "+");
   var link = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}`;
 
@@ -17,29 +16,22 @@ const getGeo = async function(address) {
   return data.results[0].geometry.location;
 };
 
-const findSpace = async function(long, lat) {
-  var apiLink =
-    "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" +
-    long +
-    "," +
-    lat +
-    "&rankby=distance&type=" +
-    areaType +
-    "&key=AIzaSyBOPU0UEp-54JoPFiG3KgcrwxcczUiYNQI";
+const findSpace = async function (long, lat) {
+  var apiLink = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + long + "," + lat + "&rankby=distance&type=" + areaType + "&key=AIzaSyBOPU0UEp-54JoPFiG3KgcrwxcczUiYNQI";
 
   var result = await fetch(apiLink);
   let data = await result.json();
   return data.results[0].geometry.location;
-};
+}
 
 module.exports = {
   /* ------------------ Creating a group ------------------ */
-  createGroup: async function(req, res) {
+  createGroup: async function (req, res) {
     var name = req.body.name;
     var geoLocation = await getGeo(req.body.location);
     var generatedCode = Math.floor(100000 + Math.random() * 900000);
 
-    Codes.find({ code: generatedCode }, "code", function(err, docs) {
+    Codes.find({ code: generatedCode }, "code", function (err, docs) {
       while (docs.length != 0) {
         generatedCode = Math.floor(100000 + Math.random() * 900000);
       }
@@ -62,30 +54,15 @@ module.exports = {
   },
 
   /* ------------------ Joining a group ------------------- */
-  joinGroup: async function(req, res) {
-    var name = req.body.name;
-    var code = parseInt(req.body.code);
-    var geoLocation = await getGeo(req.body.location);
+  joinGroup: (req, res) => {
+    var getName = req.body.name;
+    var getCode = req.body.code;
 
-    Codes.find({ code: code }, (err, result) => {
-      if (result.length > 0) {
-        var newUser = new Users({
-          name: name,
-          code: code,
-          location: geoLocation
-        });
-
-        newUser.save().then(() => res.json(newUser));
-      } else {
-        res.json({
-          message: "Code does not exist"
-        });
-      }
-
+    Users.find({}).then((err, res) => {
       if (err) {
-        res.json({
-          message: "Error"
-        });
+        console.log(err);
+      } else {
+        res.json();
       }
     });
   },
@@ -94,22 +71,44 @@ module.exports = {
   refreshGroup: (req, res) => {
     var getCode = req.body.code;
 
-    Users.find({ code: getCode }, function(err, result) {
-      var users = result;
-
-      MeetingPlace.find({ code: getCode }).then(result => {
-        if (result.length > 0) {
-          res.json({
-            users: users,
-            meetingPlace: result
-          });
-        } else {
-          res.json({
-            users: users,
-            meetingPlace: null
-          });
-        }
-      });
+    Users.find({ code: getCode }, function (err, docs) {
+      res.json(docs);
     });
+  },
+
+  findNearestPlace: (req, res) => {
+    var longitude = req.body.lng;
+    var latitude = req.body.lat;
+    var areaType = req.body.search;
+    var groupNumber = req.body.code;
+
+    var apiLink =
+      "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" +
+      longitude +
+      "," +
+      latitude +
+      "&rankby=distance&type=" +
+      areaType +
+      "&key=AIzaSyBOPU0UEp-54JoPFiG3KgcrwxcczUiYNQI";
+
+
+    fetch(apiLink).then(response => response.json()).then(data => {
+      if (data.results.length == 0) {
+        res.json([]);
+        return;
+      }
+      var newMeetingPlace = new MeetingPlace({
+        code: groupNumber,
+        location: data.results[0].geometry.location
+      })
+      newMeetingPlace.save().then(() => {
+        let tempObj = {
+          location: data.results[0].geometry.location,
+          name: data.results[0].name,
+          address: data.results[0].vicinity
+        }
+        res.json(tempObj)
+      })
+    })
   }
 };
